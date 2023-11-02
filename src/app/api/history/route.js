8e415e0ae514ecc7d51780cc3a82a23797d5b1c3
Arnz3 <cuyversarno@gmail.com>
2023-11-02 21:05:06 +0100

    Fix: hisory -> history

--- src/app/api/history/route.js
+++ src/app/api/history/route.js
@@ -3,7 +3,7 @@ import executeQuery from "@/lib/db";
 
 export async function GET(){
   const history = await executeQuery(
-    "SELECT * FROM hisory"
+    "SELECT * FROM history"
   );
 
   return Response.json(history);
