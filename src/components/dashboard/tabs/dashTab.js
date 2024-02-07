d6772931448f9bfb2c7bfffc152558cab373f184
Arnz3 <cuyversarno@gmail.com>
2024-02-07 22:45:09 +0100

    feat: bericht bij geen reserveringen

--- src/components/dashboard/tabs/dashTab.js
+++ src/components/dashboard/tabs/dashTab.js
@@ -3,6 +3,14 @@ import React from 'react'
 import ResvCard from '../card';
 
 export default function DashTab({ reserveringen, handleInfo, handleMail, goedkeuren, weigeren }) {
+  if(reserveringen.length === 0){
+    return(
+      <div>
+        Geen Reserveringen
+      </div>
+    )
+  }
+
   return (
     <div className='gap-5 grid sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-2 p-3 h-full'>
       {reserveringen?.map((reserv) => (
